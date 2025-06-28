80424cc0c9376bae4d9e6742bf25f1f528851ce4
dvrk-dvys <ja.harr91@gmail.com>
2025-06-28 14:15:07 +0200

    module 2 and homeworks

--- module_2/scratch.py
+++ module_2/scratch.py
@@ -3,4 +3,6 @@ if starting fresh remove the previous database:
 > rm mlflow.db
 
 > mlflow ui --backend-store-uri sqlite:///mlflow.db
+homeworks:
+https://github.com/RuiFSP/mlops-zoomcamp-2025/blob/master/02-experiment-tracking/homework02.ipynb
 """
